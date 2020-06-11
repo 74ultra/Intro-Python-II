2b356012087bf12c2877e9e09a3c19bcb1f4ce4d
74ultra <r.barnhill@protonmail.com>
2020-06-11 12:49:31 -0700

    Initial setup

--- src/adv.py
+++ src/adv.py
@@ -1,45 +1,47 @@
 from room import Room
+from player import Player
 
-# Declare all the rooms
+# Rooms
 
-room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
-}
-
-
+outside = Room("Outside Cave Entrance", "North of you, the cave mouth beckons")
+foyer = Room(
+    "Foyer", "Dim light filters in from the south. Dusty passages run north and east.")
+overlook = Room("Grand Overlook", "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.")
+narrow = Room("Narrow Passage",
+              "The narrow passage bends here from west to north. The smell of gold permeates the air.")
+treasure = Room("Treasure Chamber", "You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south.")
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+
+outside.n_to = foyer
+foyer.s_to = outside
+foyer.n_to = overlook
+foyer.e_to = narrow
+overlook.s_to = foyer
+narrow.w_to = foyer
+narrow.n_to = treasure
+treasure.s_to = narrow
 
 #
 # Main
 #
 
+player_name = input(
+    "Welcome to whatever this game is. Please enter your name: ")
+player = Player(player_name, current_room=outside)
+print(f'Welcome, {player.name}. Let\'s begin the journey.')
 # Make a new player object that is currently in the 'outside' room.
-
 # Write a loop that:
+while True:
+    print(
+        f'Where you are: {player.current_room.name}\n{player.current_room.description}')
+    direction = input(
+        'Enter the direction you would like to move or enter \'q\' to quit\nn --> north\ns --> south\ne --> east\nw --> west\n')
+    if direction == 'q':
+        break
+    elif direction == 'w':
+        print(player.current_room.w_to)
+    break
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
@@ -49,3 +51,56 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+# Declare all the rooms
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# room = {
+#     'outside':  Room("Outside Cave Entrance",
+#                      "North of you, the cave mount beckons"),
+
+#     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+# passages run north and east."""),
+
+#     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+# into the darkness. Ahead to the north, a light flickers in
+# the distance, but there is no way across the chasm."""),
+
+#     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+# to north. The smell of gold permeates the air."""),
+
+#     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+# chamber! Sadly, it has already been completely emptied by
+# earlier adventurers. The only exit is to the south."""),
+# }
+
+# room['outside'].n_to = room['foyer']
+# room['foyer'].s_to = room['outside']
+# room['foyer'].n_to = room['overlook']
+# room['foyer'].e_to = room['narrow']
+# room['overlook'].s_to = room['foyer']
+# room['narrow'].w_to = room['foyer']
+# room['narrow'].n_to = room['treasure']
+# room['treasure'].s_to = room['narrow']
